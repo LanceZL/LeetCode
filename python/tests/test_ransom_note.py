40c2494188ab9ac3889abea1871705830d5de601
slipper <lancezhongl@gmail.com>
2024-06-17 11:05:34 +0900

    Solve problem 383.

---
+++ python/tests/test_ransom_note.py
@@ -0,0 +1,18 @@
+from python.solutions.ransom_note import RansomNote
+from python.utils.test_case import Case
+
+
+def test():
+    testcases = [
+        Case(params=["a", "b"], expect=False),
+        Case(params=["aa", "ab"], expect=False),
+        Case(params=["acajghs", "bajsfgh"], expect=False),
+        Case(params=["aa", "aab"], expect=True),
+        Case(params=["abcd", "dbcsa"], expect=True),
+    ]
+
+    for testcase in testcases:
+        assert (
+            RansomNote().canConstruct(testcase.params[0], testcase.params[1])
+            == testcase.expect
+        )
